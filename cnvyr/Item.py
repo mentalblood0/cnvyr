00dea7384b2b8208257a5639642b63d356281465
mentalblood <neceporenkostepan@gmail.com>
2024-09-05 17:16:11 +0300

    better type checking failed error message

--- cnvyr/Item.py
+++ cnvyr/Item.py
@@ -11,4 +11,4 @@ class Item:
     def __post_init__(self):
         for f in dataclasses.fields(self):
             if f.type | type(value := getattr(self, f.name)) != f.type:
-                raise ValueError(f"{f.name}={value}")
+                raise ValueError(f"{self.__class__.__name__}.{f.name} expects value of type {f.type}, got {value}")
